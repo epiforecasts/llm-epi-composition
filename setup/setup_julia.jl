8ced7621b2e07fd2f2f8a0f72851a83d2f87a510
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2025-12-15 23:40:53 +0000

    Fix Julia setup script to check packages properly

--- setup/setup_julia.jl
+++ setup/setup_julia.jl
@@ -20,20 +20,19 @@ registry_packages = [
 
 println("Installing packages from registry...")
 for pkg in registry_packages
-    try
-        @eval using $(Symbol(pkg))
-        println("  $pkg: already installed")
-    catch
+    if Base.find_package(pkg) === nothing
         println("  Installing $pkg...")
         Pkg.add(pkg)
+    else
+        println("  $pkg: already installed")
     end
 end
 
-# EpiAware from GitHub (not in General registry)
+# EpiAware from GitHub (CDCgov repo)
 println("\nInstalling EpiAware from GitHub...")
 if Base.find_package("EpiAware") === nothing
     println("  Installing EpiAware...")
-    Pkg.add(url="https://github.com/CDCgov/Rt-without-renewal", subdir="EpiAware")
+    Pkg.add(url="https://github.com/CDCgov/Rt-without-renewal.git", subdir="EpiAware")
 else
     println("  EpiAware: already installed")
 end
